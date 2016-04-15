b77bbcb723b0f74bb0141adc8fb097cc3aecf57a
M. Navrotskiy <m.navrotskiy@gmail.com>
2016-04-15 20:20:27 +0300

    add parsing lod.ifmo

--- app.js
+++ app.js
@@ -7,6 +7,10 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -17,6 +21,35 @@ var app = express();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.post('/q.json', urlencodedParser, function(req, res) {
+  console.log(req.body);
+  var url = req.body.url;
+  var json = {title: ''};
+
+  request(url, function(error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+
+      $('#title').filter(function() {
+        var data = $(this);
+
+        json.title = data.text();
+        console.log(json);
+        res.send(json);
+      });
+    }
+  });
+
+  // console.log(json);
+  // res.send(json);
+});
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
--- public/js/form_component.js
+++ public/js/form_component.js
@@ -2,6 +2,30 @@ $(document).ready(function(){
   console.log('render component');
 
   window.FormComponent = React.createClass({
+    getInitialState: function() {
+      return {url: 'http://lod.ifmo.ru/page/Laboratory87847'};
+    },
+
+    handleClick: function(event) {
+      console.log(this.state.url);
+      $.ajax({
+        url: '/q.json',
+        dataType: 'json',
+        data: {url: this.state.url},
+        type: 'POST',
+        success: function(data) {
+          console.log(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log(textStatus);
+        }
+      });
+    },
+
+    handleChange: function(event) {
+      this.setState({url: event.target.value});
+    },
+
     render: function() {
       return (
         <div>
@@ -11,13 +35,15 @@ $(document).ready(function(){
                 <div className="form-group is-empty">
                   <label htmlFor="url" className="col-md-2 control-label">Url to iFMO.LOD page</label>
                   <div className="col-md-10">
-                    <input className="form-control" id="url" placeholder="LOD url" type="text" />
+                    <input className="form-control" id="url" placeholder="LOD url"
+                           type="text" onChange={this.handleChange}
+                           value={this.state.url}/>
                   </div>
                 </div>
                 <div className="form-group">
                   <div className="col-md-10 col-md-offset-2">
-                    <button type="button" className="btn btn-default">Cancel</button>
-                    <button type="button" className="btn btn-primary">Submit</button>
+                    <button type="button" className="btn btn-primary"
+                            onClick={this.handleClick}>Submit</button>
                   </div>
                 </div>
               </fieldset>
